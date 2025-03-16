4dcd5c24f60f491b6bc6aabb748eddaf5ba94c23
W A D G H Athukorala <139699847+GimanthaHesh@users.noreply.github.com>
2025-03-16 07:53:00 +0000

    Created a file to reverse numbers from two sides at one time

---
+++ ReverseSides.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ReverseSides {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        scanner.close();
+
+        int middle = number/2;
+        for (int i = 1; i <= number; i++ ){
+        if(i <= middle)
+        {
+          System.out.print((middle-i+1)+" ");  
+        }
+        else
+        {
+           if(i == middle + 1)  
+           {
+              System.out.print(i+" ");  
+           }
+           else
+           {
+              System.out.print((number - i+ middle + 2) +" "); 
+           }
+        }
+        }
+        System.out.println();
+    }
+}
